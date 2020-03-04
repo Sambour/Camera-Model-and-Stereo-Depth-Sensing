2e99b4a9cb18376c655d977d1a96b03f6f0f676d
Anubhav Guha <anubhab.97guha@gmail.com>
2020-03-04 15:50:45 -0700

    Update task4.py

--- code/task_4/task4.py
+++ code/task_4/task4.py
@@ -6,11 +6,39 @@ img_l = cv2.imread('../../images/task_3_and_4/left_0.png')
 img_r = cv2.imread('../../images/task_3_and_4/right_0.png')
 
 
-gray_l = cv2.cvtColor(img_l, cv2.COLOR_BGR2GRAY)
-gray_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
+#gray_l = cv2.cvtColor(img_l, cv2.COLOR_BGR2GRAY)
+#gray_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
+
+fs_l = cv2.FileStorage("../../parameters/left_camera_intrinsics.xml", cv2.FILE_STORAGE_READ)
+cameraMatrix_l = fs_l.getNode("camera_intrinsic")
+#print(cameraMatrix_l.mat())
+distMatrix_l = fs_l.getNode("distort_coefficients")
+
+fs_r = cv2.FileStorage("../../parameters/right_camera_intrinsics.xml", cv2.FILE_STORAGE_READ)
+cameraMatrix_r = fs_r.getNode("camera_intrinsic")
+distMatrix_r = fs_r.getNode("distort_coefficients")
 
 fs = cv2.FileStorage("../../parameters/stereo_rectification.xml", cv2.FILE_STORAGE_READ)
 disparityMatrix = fs.getNode("disparity_depth_matrix")
+
+# Undistort left image
+h, w = img_l.shape[:2]
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix_l.mat(), distMatrix_l.mat(), (w, h), 1, (w, h))
+mapx, mapy = cv2.initUndistortRectifyMap(cameraMatrix_l.mat(), distMatrix_l.mat(), None, newcameramtx, (w, h), 5)
+dst = cv2.remap(img_l, mapx, mapy, cv2.INTER_LINEAR)
+x, y, w, h = roi
+dst = dst[y:y + h, x:x + w]
+gray_l = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+
+# Undistort right image
+h, w = img_r.shape[:2]
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix_r.mat(), distMatrix_r.mat(), (w, h), 1, (w, h))
+mapx, mapy = cv2.initUndistortRectifyMap(cameraMatrix_r.mat(), distMatrix_r.mat(), None, newcameramtx, (w, h), 5)
+dst = cv2.remap(img_l, mapx, mapy, cv2.INTER_LINEAR)
+x, y, w, h = roi
+dst = dst[y:y + h, x:x + w]
+gray_r = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+
 
 stereo = cv2.StereoBM_create(numDisparities=16, blockSize=13)  
 disparity = stereo.compute(gray_l,gray_r)
